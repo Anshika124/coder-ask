029dd3a31db14ed17540e044d91a7487c58e5748
Anshika Prajapati <anshikaprajapati124@gmail.com>
2024-07-17 22:54:44 +0530

    Added text editer with react quilljs

--- src/pages/Ask.jsx
+++ src/pages/Ask.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { useQuill } from 'react-quilljs';
+import 'quill/dist/quill.snow.css'; 
+import '../css/Ask.css'
 
 function Ask() {
+  const { quill, quillRef } = useQuill();
+
+  console.log(quill);    
+  console.log(quillRef); 
+
   return (
-    <div>Ask</div>
-  )
+    <div className='askMainContainer'>
+      <div className="askContainer" style={{ width: 770, height: 300 }}>
+      <div ref={quillRef} />
+    </div>
+    </div>
+  );
 }
 
 export default Ask
