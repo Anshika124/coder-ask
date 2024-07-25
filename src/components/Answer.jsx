4e72fdf9512ca9eb0d4de961584a9733dd0c8fd6
Anshika Prajapati <anshikaprajapati124@gmail.com>
2024-07-25 17:15:50 +0530

    Anwser card posted date updated

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 const Answer = ({ answer }) => {
   return (
-    <div key={answer._id} className="answer-card" style={{ margin: '1rem 0', padding: '1rem', border: '1px solid #ddd', borderRadius: '5px' }}>
+    <div key={answer._id} className="answer-card" style={{ margin: '1rem 0', padding: '1rem',  borderBottom: '1px solid #ddd' }}>
       <h2>{answer.answerFor.title}</h2>
       <p><strong>Question Description:</strong> {answer.answerFor.description}</p>
       <p><strong>Answer:</strong> {answer.description}</p>
--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import { convert } from 'html-to-text';
+import moment from 'moment/moment';
 
 function Question({ question }) {
+
+  let descriptionPreview = convert(question.description);
+  let desc = descriptionPreview.split(' ').slice(0,15).join(' ')
+  if (desc.length > 150) {
+    desc = desc.slice(0,150)
+  }
   return (
     <div className="question" style={{ padding: '1rem', borderBottom: '1px solid #ddd' }}>
       <h2>{question.title}</h2>
-      <div dangerouslySetInnerHTML={{ __html: question.description }}></div>
-      <p><strong>Posted On:</strong> {new Date(question.postedOn).toLocaleString()}</p>
+      <div>{ desc }</div>
+      <p><strong>Posted:</strong> {moment(new Date(question.postedOn).toLocaleString()).fromNow()}</p>
       <p><strong>Posted By:</strong> {new String(question.postedBy.userName).toLocaleString()}</p>
+      <p><strong>Answered:</strong> {question.answersList.length}</p>
+
     </div>
   );
 }
