aa0d85e35b2916f6c825da4e1cf7fd587c365da2
Anshika Prajapati <anshikaprajapati124@gmail.com>
2024-08-20 23:20:26 +0530

    updated local db

--- src/controller/URLManager.js
+++ src/controller/URLManager.js
@@ -1,4 +1,4 @@
 export const logoUrl = "https://img.icons8.com/fluency/48/why-us-female.png"
 export const profileUrl = "https://img.icons8.com/doodle/48/girl.png"
-export const localDBUrl = "http://localhost:5000"
-// export const localDBUrl = "https://coder-ask-server.onrender.com"
+// export const localDBUrl = "http://localhost:5000"
+export const localDBUrl = "https://coder-ask-server.onrender.com"
