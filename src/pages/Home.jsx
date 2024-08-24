75f35f25af62d37126a17c6c1a651eba14dae4b4
Anshika Prajapati <anshikaprajapati124@gmail.com>
2024-08-24 15:58:06 +0530

    Home page UI Improved

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';import { localDBUrl } from '../controller/URLManager'
+import axios from 'axios';
+import { localDBUrl } from '../controller/URLManager'
 import Question from '../components/Question';
 import Loading from '../components/Loading';
 import '../css/Home.css';
--- src/pages/Questions.jsx
+++ src/pages/Questions.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { localDBUrl } from '../controller/URLManager'
 import Question from '../components/Question';
 import Loading from '../components/Loading';
+import '../css/Home.css';
 
 function Questions() {
   
@@ -30,12 +31,7 @@ function Questions() {
     }
 
   return (
-    <div className="container" style={{
-      padding: '2rem',
-      maxWidth: '70vw',
-      margin: 'auto',
-      boxShadow: 'rgba(0, 0, 0, 0.1) 0px 4px 8px'
-       }}>
+    <div className="container Home" >
       <header className="header">
         <h1>Questions</h1>
       </header>
