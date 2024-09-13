eceb9875f4062b3ec281928c00228101d7e7a1d4
Anshika Prajapati <anshikaprajapati124@gmail.com>
2024-09-13 22:54:20 +0530

    Added message for loading

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -4,8 +4,11 @@ import ReactLoading from 'react-loading'
 
 function Loading() {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: 'calc(100vh - 0px)' }}><ReactLoading type={'spin'} color={'#ffffff'} height={'50px'} width={'50px'} /></div>
-    
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: 'calc(100vh - 0px)', flexDirection: 'column', gap:'5px' }}>
+      <ReactLoading type={'spin'} color={'#ffffff'} height={'50px'} width={'50px'} />
+      <p>Wait while fetching data..</p>
+    </div>
+
   )
 }
 
